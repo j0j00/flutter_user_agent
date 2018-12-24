42416c460261e0ff41ab084ea348f4d5bcf8a0ee
j0j00 <45653919+j0j00@users.noreply.github.com>
2018-12-24 21:18:09 +0000

    Code clean up for official release.

--- lib/flutter_user_agent.dart
+++ lib/flutter_user_agent.dart
@@ -3,28 +3,33 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterUserAgent {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_user_agent');
+  static const MethodChannel _channel = MethodChannel('flutter_user_agent');
 
   static Map<String, dynamic> _properties;
 
+  /// Initialize the module.
+  /// This should be called before the module can be used.
   static Future init() async {
     _properties = Map.unmodifiable(await _channel.invokeMethod('getProperties'));
   }
 
+  /// Returns the device's user agent.
   static String get userAgent {
     return _properties['userAgent'];
   }
 
+  /// Returns the device's webview user agent.
   static String get webViewUserAgent {
     return _properties['webViewUserAgent'];
   }
 
-  static getProperty(String property) {
+  /// Fetch a [property] that can be used to build your own user agent string.
+  static dynamic getProperty(String property) {
     return _properties[property];
   }
 
-  static get properties {
+  /// Return a map of properties that can be used to generate the user agent string.
+  static Map<String, dynamic> get properties {
     return _properties;
   }
 }
