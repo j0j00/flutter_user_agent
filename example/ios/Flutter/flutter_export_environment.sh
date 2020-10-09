8ce3fb5ed906ad82167725d23ca92f8558e0c80c
Zahid Shaikh <zahid.shaikh@tecocraft.com>
2020-10-09 23:21:08 +0530

    Migrated to androidx and example project compileSdkVersion changed to 29 from 27.

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=E:\flutter_sdk\flutter_windows_1.17.5-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\open_source_github\flutter_user_agent\example"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=E:\flutter_sdk\flutter_windows_1.17.5-stable\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
