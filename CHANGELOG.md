5cdd6185e12ea7935980cce15d0d566d5891e007
v7lin <v7lin@qq.com>
2021-02-22 17:14:01 +0800

    Update 'flutterEmbedding' to 2.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,9 @@
 # flutter_user_agent version history
+
+## 1.2.3
+
+* Update 'flutterEmbedding' to 2.
+
 ## 1.2.2
 
 * Update `compileSdkVersion` to 28. (thanks [@miguelpruivo](https://github.com/miguelpruivo)!)
--- android/src/main/java/io/jojodev/flutter/flutteruseragent/FlutterUserAgentPlugin.java
+++ android/src/main/java/io/jojodev/flutter/flutteruseragent/FlutterUserAgentPlugin.java
@@ -8,109 +8,123 @@ import android.os.Build.VERSION_CODES;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
+import androidx.annotation.NonNull;
+
 import java.util.HashMap;
 import java.util.Map;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** FlutterUserAgentPlugin */
-public class FlutterUserAgentPlugin implements MethodCallHandler {
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_user_agent");
-    channel.setMethodCallHandler(new FlutterUserAgentPlugin(registrar.context()));
-  }
+/**
+ * FlutterUserAgentPlugin
+ */
+public class FlutterUserAgentPlugin implements FlutterPlugin, MethodCallHandler {
+    private MethodChannel channel;
+    private Context applicationContext;
 
-  private final Context context;
-  private Map<String, Object> constants;
+    private Map<String, Object> constants;
 
-  private FlutterUserAgentPlugin(Context context) {
-    this.context = context;
-  }
+    // --- FlutterPlugin
 
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getProperties")) {
-      result.success(getProperties());
-    } else {
-      result.notImplemented();
-    }
-  }
-
-  private Map<String, Object> getProperties() {
-    if(constants != null) {
-      return constants;
-    }
-    constants = new HashMap<>();
-
-    PackageManager packageManager = this.context.getPackageManager();
-    String packageName = this.context.getPackageName();
-    String shortPackageName = packageName.substring(packageName.lastIndexOf(".") + 1);
-    String applicationName = "";
-    String applicationVersion = "";
-    int buildNumber = 0;
-    String userAgent = this.getUserAgent();
-    String packageUserAgent = userAgent;
-
-    try {
-      PackageInfo info = packageManager.getPackageInfo(packageName, 0);
-      applicationName = this.context.getApplicationInfo().loadLabel(this.context.getPackageManager()).toString();
-      applicationVersion = info.versionName;
-      buildNumber = info.versionCode;
-      packageUserAgent = shortPackageName + '/' + applicationVersion + '.' + buildNumber + ' ' + userAgent;
-
-    } catch(PackageManager.NameNotFoundException e) {
-      e.printStackTrace();
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+      channel = new MethodChannel(binding.getBinaryMessenger(), "flutter_user_agent");
+      channel.setMethodCallHandler(this);
+      applicationContext = binding.getApplicationContext();
     }
 
-    constants.put("systemName", "Android");
-    constants.put("systemVersion", Build.VERSION.RELEASE);
-    constants.put("packageName", packageName);
-    constants.put("shortPackageName", shortPackageName);
-    constants.put("applicationName", applicationName);
-    constants.put("applicationVersion", applicationVersion);
-    constants.put("applicationBuildNumber", buildNumber);
-    constants.put("packageUserAgent", packageUserAgent);
-    constants.put("userAgent", userAgent);
-    constants.put("webViewUserAgent", this.getWebViewUserAgent());
-
-    return constants;
-  }
-
-  private String getUserAgent() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-      return System.getProperty("http.agent");
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+      channel.setMethodCallHandler(null);
+      channel = null;
+      applicationContext = null;
     }
 
-    return "";
-  }
+    // --- MethodCallHandler
 
-  private String getWebViewUserAgent() {
-    if (Build.VERSION.SDK_INT >= VERSION_CODES.JELLY_BEAN_MR1) {
-      return WebSettings.getDefaultUserAgent(this.context);
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        if ("getProperties".equals(call.method)) {
+            result.success(getProperties());
+        } else {
+            result.notImplemented();
+        }
     }
 
-    WebView webView = new WebView(this.context);
-    String userAgentString = webView.getSettings().getUserAgentString();
+    private Map<String, Object> getProperties() {
+        if (constants != null) {
+            return constants;
+        }
+        constants = new HashMap<>();
 
-    this.destroyWebView(webView);
+        PackageManager packageManager = applicationContext.getPackageManager();
+        String packageName = applicationContext.getPackageName();
+        String shortPackageName = packageName.substring(packageName.lastIndexOf(".") + 1);
+        String applicationName = "";
+        String applicationVersion = "";
+        int buildNumber = 0;
+        String userAgent = getUserAgent();
+        String packageUserAgent = userAgent;
 
-    return userAgentString;
-  }
+        try {
+            PackageInfo info = packageManager.getPackageInfo(packageName, 0);
+            applicationName = applicationContext.getApplicationInfo().loadLabel(applicationContext.getPackageManager()).toString();
+            applicationVersion = info.versionName;
+            buildNumber = info.versionCode;
+            packageUserAgent = shortPackageName + '/' + applicationVersion + '.' + buildNumber + ' ' + userAgent;
 
-  private void destroyWebView(WebView webView) {
-    webView.loadUrl("about:blank");
-    webView.stopLoading();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
 
-    webView.clearHistory();
-    webView.removeAllViews();
-    webView.destroyDrawingCache();
+        constants.put("systemName", "Android");
+        constants.put("systemVersion", Build.VERSION.RELEASE);
+        constants.put("packageName", packageName);
+        constants.put("shortPackageName", shortPackageName);
+        constants.put("applicationName", applicationName);
+        constants.put("applicationVersion", applicationVersion);
+        constants.put("applicationBuildNumber", buildNumber);
+        constants.put("packageUserAgent", packageUserAgent);
+        constants.put("userAgent", userAgent);
+        constants.put("webViewUserAgent", getWebViewUserAgent());
 
-    // NOTE: This can occasionally cause a segfault below API 17 (4.2)
-    webView.destroy();
-  }
+        return constants;
+    }
+
+    private String getUserAgent() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            return System.getProperty("http.agent");
+        }
+
+        return "";
+    }
+
+    private String getWebViewUserAgent() {
+        if (Build.VERSION.SDK_INT >= VERSION_CODES.JELLY_BEAN_MR1) {
+            return WebSettings.getDefaultUserAgent(applicationContext);
+        }
+
+        WebView webView = new WebView(applicationContext);
+        String userAgentString = webView.getSettings().getUserAgentString();
+
+        destroyWebView(webView);
+
+        return userAgentString;
+    }
+
+    private void destroyWebView(WebView webView) {
+        webView.loadUrl("about:blank");
+        webView.stopLoading();
+
+        webView.clearHistory();
+        webView.removeAllViews();
+        webView.destroyDrawingCache();
+
+        // NOTE: This can occasionally cause a segfault below API 17 (4.2)
+        webView.destroy();
+    }
 }
